584952692cdb0b97c685323a0b80ae5241eba7c1
AlissonBarbosa <alisson.89.sud@gmail.com>
2018-01-15 16:01:29 -0300

    App pregoeiro iniciada

--- pregoeiro/models.py
+++ pregoeiro/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class PregoeiroManager(models.Manager):
+
+	def search(self, query):
+		return self.get_queryset.filter(models.Q(nome__icontains=query | models.Q(matricula__icontains=query)))
+
+
+class Pregoeiro(models.Model):
+
+	nome = models.CharField('Nome', max_length=100)
+	senha = models.CharField('Senha', max_length=10)
+	matricula = models.CharField('Matricula', max_length=10)
+	portaria = models.CharField('Portaria', max_length=20)
+	endereco = models.CharField('Endereço', max_length=100)
+	cidade = models.CharField('Cidade', max_length=20)
+	cep = models.CharField('CEP',max_length=9)
+	estado = models.CharField('Estado', max_length=20)
+	telefone = models.CharField('Telefone', max_length=12)
+	email = models.EmailField('Email')
+	cpf = models.CharField('CPF', max_length=14)
+	rg = models.CharField('RG', max_length=8)
