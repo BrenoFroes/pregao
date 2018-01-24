f2960a6949df3f600d5608606b56cc50a3675705
ely <ely.richardson@gnail.com>
2018-01-24 02:09:51 +0000

    adiconado cadastro de fornecedores

--- pregao/urls.py
+++ pregao/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
 	path('', include('core.urls')),
 	path('users/', include('users.urls')),
 	path('users/', include('django.contrib.auth.urls')),
+    path('fornecedores/', include('fornecedores.urls')),
     path('admin/', admin.site.urls),
 ]
